af1b8206fdff8cd035825a848316cb0bf449330f
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-02-09 14:20:34 -0500

    Updated layout with font

--- wedding/src/app/layout.js
+++ wedding/src/app/layout.js
@@ -1,11 +1,31 @@
-import Navbar from '../components/Navbar';
 import '../globals.css';
+import { Charm, Nunito } from "next/font/google";
+import Navbar from '../components/Navbar';
+
+const charm = Charm({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-charm",
+});
+
+const nunito = Nunito({
+  weight: ["200", "400", "700", "1000"],
+  subsets: ["latin"],
+  variable: "--font-nunito", b
+});
+
+export const metadata = {
+  title: "Charlotte and Christian's Wedding Website",
+  description: "A beautiful website for the wedding.",
+};
 
 export default function Layout({ children }) {
   return (
-    <div>
-      <Navbar />
-      <main className="pt-16">{children}</main>
-    </div>
+    <html lang="en" className={`${charm.variable} ${nunito.variable}`}>
+      <body>
+        <Navbar />
+        {children}
+      </body>
+    </html>
   );
 }
